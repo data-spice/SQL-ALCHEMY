5b0291abccd0fd4c82634f0e82cf150e1dd94233
data-spice <myvicgit@gmail.com>
2026-04-27 13:39:42 +0300

    Finalized on postgres 1

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from models import User, engine
-from sqlalchemy.orm import sessionmaker
+# from models import User, engine
+# from sqlalchemy.orm import sessionmaker
 
-Session=sessionmaker(bind=engine)
-session=Session()
+# Session=sessionmaker(bind=engine)
+# session=Session()
 # user=User(name="John Doe",age=30)
 # user_2=User(name="Victor Mwaniki",age=45)
 # user_3=User(name="Shantel Wanjiku",age=28)
@@ -22,7 +22,30 @@ session=Session()
 
 # user=session.query(User).filter_by(age=10).one_or_none()
 
-user=session.query(User).filter_by(id=1).first()
+# user=session.query(User).filter_by(id=1).first()
 
-session.delete(user)
-session.commit()
+# session.delete(user)
+# session.commit()
+
+# ==========================================================
+#                     LESSON 3
+# ==========================================================
+import random
+from sqlalchemy.orm import sessionmaker
+from models import User,engine
+
+Session=sessionmaker(bind=engine)
+session=Session()
+
+# names=["Victor","Mwaniki","Alvin", "Kipchumba","Zenin" ,"Rocho","Frida" ,"Mohammed"]
+# ages=[21,34,21,34,67,43,61,52,34,10]
+
+# for x in range(20):
+#     user=User(name=random.choice(names),age=random.choice(ages))
+#     session.add(user)
+
+# session.commit()
+
+users=session.query(User).order_by(User.age).all()
+for user in users:
+    print(f"User age: {user.age}, name: {user.name}, id: {user.id}")
